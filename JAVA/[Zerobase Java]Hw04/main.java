c664ce8b75110df884a2a0e9c36720d4a8c4668d
원해연 <73468962+HaeYeon-Won@users.noreply.github.com>
2022-03-07 18:23:19 +0900

    Create main.java

---
+++ JAVA/[Zerobase Java]Hw04/main.java
@@ -0,0 +1,12 @@
+public class main {
+
+   public static void main(String[] args) {
+      // TODO Auto-generated method stub
+      long totalCount = 127;
+      long pageIndex = 7;
+      
+      Pager pager = new Pager(totalCount);
+      System.out.println(pager.html(pageIndex));
+   }
+
+}
