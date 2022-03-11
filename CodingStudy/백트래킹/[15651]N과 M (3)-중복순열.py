3c70239c0a5e33f4278c81c378953bf24ed9a423
원해연 <73468962+HaeYeon-Won@users.noreply.github.com>
2022-03-11 15:47:36 +0900

    Update [15651]N과 M (3)-중복순열.py

--- CodingStudy/백트래킹/[15651]N과 M (3)-중복순열.py
+++ CodingStudy/백트래킹/[15651]N과 M (3)-중복순열.py
@@ -18,3 +18,23 @@ if __name__ =="__main__":
     n,m = map(int, input().split())
     q = deque()
     solution()
+
+    
+"""
+    - 중복 순열이란?
+중복 가능한 n개 중에 r개를 나열하는 경우의 수(순서 O)를 말한다.
+itertools의 product 함수를 이용하여 두 개 이상의 리스트에서 모든 조합을 계산할 수도 있다.
+- case1
+
+from itertools import product
+
+data1 = [1, 2, 3, 4]
+
+for x in product(data1, repeat=2):
+    print(x)
+    
+    
+# 결과
+(1, 1) (1, 2) (1, 3) (1, 4) (2, 1) (2, 2) (2, 3) (2, 4)
+(3, 1) (3, 2) (3, 3) (3, 4) (4, 1) (4, 2) (4, 3) (4, 4)
+"""
