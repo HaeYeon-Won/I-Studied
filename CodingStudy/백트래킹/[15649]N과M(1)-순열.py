9cfcec46da065382dcf7912623a609eaf2ba707e
원해연 <73468962+HaeYeon-Won@users.noreply.github.com>
2022-03-11 15:46:28 +0900

    Update [15649]N과M(1)-순열.py

--- CodingStudy/백트래킹/[15649]N과M(1)-순열.py
+++ CodingStudy/백트래킹/[15649]N과M(1)-순열.py
@@ -18,3 +18,19 @@ if __name__ =="__main__":
     n,m = map(int, input().split())
     q = deque()
     solution()
+
+    """
+    permutation
+순열이란 몇 개를 골라 순서를 고려해 나열한 경우의 수를 말한다. 즉, 서로 다른 n 개 중 r 개를 골라 순서를 정해 나열하는 가짓수이며 순열이라는 의미의 영어 ‘Permutation’의 첫 글자 P를 따서 nPr로 표시한다. 출처 : [네이버 지식백과] 순열 [Permutation, 順列] (두산백과)
+
+순열은 순서를 고려하기 때문에 [A, B, C]의 리스트에서 2개의 원소를 골라 순서를 정해 나열하면
+[(A, B), (A, C), (B, A), (B, C), (C, A), (C, B)] 가 나오게 된다. 즉 순열에서는 (A, B)와 (B, A)는 다른 것이다.
+
+import itertools
+
+arr = ['A', 'B', 'C']
+nPr = itertools.permutations(arr, 2)
+print(list(nPr))
+
+결과 : [('A', 'B'), ('A', 'C'), ('B', 'A'), ('B', 'C'), ('C', 'A'), ('C', 'B')]
+    """
