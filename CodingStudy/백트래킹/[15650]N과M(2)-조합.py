5327fcac55fe497f98a28a3fb798bcc2fa4a17c0
원해연 <73468962+HaeYeon-Won@users.noreply.github.com>
2022-03-11 15:46:48 +0900

    Update [15650]N과M(2)-조합.py

--- CodingStudy/백트래킹/[15650]N과M(2)-조합.py
+++ CodingStudy/백트래킹/[15650]N과M(2)-조합.py
@@ -19,3 +19,19 @@ if __name__ =="__main__":
     n,m = map(int, input().split())
     q = deque()
     solution(1)
+    
+"""
+combination
+조합이란 서로 다른 n개 중에서 r개(n≥r) 취하여 조를 만들 때, 이 하나하나의 조를 n개 중에서 r개 취한 조합이라고 한다. 출처 : [네이버 지식백과] 조합 (두산백과)
+
+조합은 순서를 고려하지 않기 때문에 [A, B, C]의 리스트에서 2개의 원소를 골라 나열하면
+[(A, B), (A, C), (B, C)] 가 나오게 된다. 조합은 (A, B)와 (B, A)는 같은 것으로 취급한다.
+
+import itertools
+
+arr = ['A', 'B', 'C']
+nCr = itertools.combinations(arr, 2)
+print(list(nCr))
+
+결과 : [('A', 'B'), ('A', 'C'), ('B', 'C')]
+"""
